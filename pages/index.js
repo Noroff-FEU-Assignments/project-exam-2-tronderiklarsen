ce791a92a66a775b466b807428804c9405e1ade9
Trond Erik Larsen <56018387+tronderiklarsen@users.noreply.github.com>
2021-04-30 19:11:04 +0200

    Started fetching

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Layout from "../components/layout/Layout"
 import Search from "../components/search/Search"
+import { API_URL } from "../constants/api"
 
 export default function HomePage() {
   return (
@@ -7,6 +8,17 @@ export default function HomePage() {
       <h1>Find your next accomodation!</h1>
       <h2>Hotels, B&Bs and guesthouses</h2>
       <Search />
+      <h1>Explore Bergen</h1>
+      <h2>Featured acommodations</h2>
     </Layout>
   )
+}
+
+export async function getServerSideProps({place}) {
+  const response = await fetch(`${API_URL}/places`)
+  const places = await response.json();
+
+  return {
+    props: {places},
+  }
 }
