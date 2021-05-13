6d3bb55dfdcf301ff0f65ee0e6ca72da4f1f6e2f
Trond Erik Larsen <56018387+tronderiklarsen@users.noreply.github.com>
2021-05-13 23:15:37 +0200

    Refactored contact page with hook-form and yup

--- pages/contact.js
+++ pages/contact.js
@@ -1,26 +1,43 @@
 import Layout from "../components/layout/Layout";
-import { useState } from "react";
 import { useRouter } from "next/router";
 import { API_URL } from "../constants/api";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object().shape({
+  name: yup.string().required("Please enter your name"),
+  email: yup
+    .string()
+    .required("Please enter an email address")
+    .email("Please enter a valid email address"),
+  message: yup
+    .string()
+    .required("Please enter your message")
+    .min(10, "The message must be at least 10 characters"),
+});
 
 export default function AdminPage() {
-  const [values, setValues] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
 
   const router = useRouter();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = async (data) => {
+    console.log(data);
 
     const response = await fetch(`${API_URL}/contacts`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(values),
+      body: JSON.stringify(data),
     });
 
     if (!response.ok) {
@@ -31,45 +48,40 @@ export default function AdminPage() {
     }
   };
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setValues({ ...values, [name]: value });
-  };
-
   return (
     <Layout title="Contact us - Holidaze">
       <h1>Contact us</h1>
       <h2>Got any question?</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="name">Name</label>
         <input
+          {...register("name")}
           type="text"
           id="name"
           name="name"
-          value={values.name}
-          onChange={handleInputChange}
           placeholder="Enter name"
         ></input>
+        {errors.name && <span>{errors.name.message}</span>}
 
         <label htmlFor="email">Email</label>
         <input
+          {...register("email")}
           type="email"
           id="email"
           name="email"
-          value={values.email}
-          onChange={handleInputChange}
           placeholder="Enter email"
         ></input>
+        {errors.email && <span>{errors.email.message}</span>}
 
         <label htmlFor="message">Message</label>
         <textarea
+          {...register("message")}
           type="text"
           id="message"
           name="message"
-          value={values.message}
-          onChange={handleInputChange}
           placeholder="Enter message"
         ></textarea>
+        {errors.message && <span>{errors.message.message}</span>}
 
         <input className="btn" type="submit"></input>
       </form>
